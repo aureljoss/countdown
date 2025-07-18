df063a3490e47ba6e8fb1009e22f17323e977a8c
Aurelie <josserand.n.aurelie@gmail.com>
2025-07-18 17:58:48 -0400

    Date change

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 1, 2024 00:00:00").getTime();
+var countDownDate = new Date("July 1, 2026 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
